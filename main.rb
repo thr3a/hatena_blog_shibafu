b76ea0afce972d25e6a42a9d4661c135736c9d38
thr3a <a@example.com>
2020-11-15 21:37:07 +0900

    fix: はてなブログのsitemap仕様変更対応

--- main.rb
+++ main.rb
@@ -12,15 +12,18 @@ def date_articles(blog_url)
   oldest_date = Date.today - 366
   dates = (oldest_date..Date.today).map { |e| [e.strftime("%F"), 0] }.to_h
   catch(:nested_break) do
-    5.times do |num|
-      sitemap_url =  "#{blog_url.gsub(/\/$/, "")}/sitemap.xml?page=#{num + 1}"
-      begin
-        xml = open(sitemap_url).read
-      rescue => e
-        throw :nested_break
-      else
-        lastmods = Ox.load(xml, mode: :hash_no_attrs)[:urlset][:url].map{|e| e[:lastmod]}
-        lastmods.each do |lastmod|
+    sitemap_root_url = "#{blog_url}/sitemap.xml"
+    xml = open(sitemap_root_url).read
+    site_map_urls = Ox.load(xml, mode: :hash_no_attrs)[:sitemapindex][:sitemap]
+    # 先頭は記事ではない為除外
+    1..14.times do |index|
+      site_map_url = site_map_urls[index][:loc]
+      xml = open(site_map_url).read
+      urls = Ox.load(xml, mode: :hash_no_attrs)
+      if urls[:urlset] && urls[:urlset].length > 0
+        # urlが１つだけだと配列にならないのでflatten
+        urls = [urls[:urlset][:url]].flatten.map{|e| e[:lastmod]}
+        urls.each do |lastmod|
           lastmod = Date.parse(lastmod)
           if lastmod >= oldest_date
             dates[lastmod.strftime("%F")] += 1
